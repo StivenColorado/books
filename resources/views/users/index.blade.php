df52dd9e6cc7e90d651b2434d1fefc2a5463fc9d
stiven <stivenchoo@gmail.com>
2024-04-24 17:08:18 -0500

    config middleware in web.php file

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -30,19 +30,22 @@
                                     <th scope="row">{{ $user->email }}</th>
                                     <th scope="row">
                                         @foreach ($user->roles as $role)
-                                            {{ $role->name }}
+                                            {{ $role->name }},
                                         @endforeach
                                     </th>
 
                                     <th scope="row">
-                                        <div class="d-flex">
+                                        <div class="d-flex justify-content-center">
                                             <a href="{{ route('users.edit', ['user' => $user->id]) }}"
-                                                class="btn btn-warning btn-sm">Editar</a>
+                                                class="btn btn-warning btn-sm">
+                                                <i class="fas fa-pencil-alt"></i></a>
                                             <form action="{{ route('users.destroy', ['user' => $user->id]) }}"
                                                 method="post">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button class="ms-2 btn btn-danger btn-sm">Eliminar</button>
+                                                <button class="ms-2 btn btn-danger btn-sm">
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </button>
                                             </form>
                                         </div>
                                     </th>
