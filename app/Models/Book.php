6e3d8bedccff6b844b48e29638581aade9d8c0de
stiven <stivenchoo@gmail.com>
2024-04-25 17:54:17 -0500

    book image

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use App\Models\Lend;
 use App\Models\Author;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
@@ -21,7 +23,17 @@ class Book extends Model
         'description',
     ];
 
-
+    protected $appends = ['format_description'];
+    public function formatDescription(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value,$attributes){
+                return Str::limit($attributes['description'], 80, '...');
+            }
+            ,
+            // set:fn ($value) => Str::upper($value)
+         );
+    }
 
     /*
 		Book::with('category','author')->get();
